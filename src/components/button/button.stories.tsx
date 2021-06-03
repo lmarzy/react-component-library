8dc2caffc3f87aa2e3cd2a2d7967dfebef11635a
lmarzy <me@phz3.net>
2021-06-03 16:13:16 +0100

    Updates to structure

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { Button } from '../button/button';
+import { Button } from './button';
 
 export default {
   title: 'Components/Button',
